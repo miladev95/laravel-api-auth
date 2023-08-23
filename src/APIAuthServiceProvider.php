5d460aa8fcd52dc04a4d015135cabdd3ae36c4dc
Milad <miladev95@gmail.com>
2023-08-23 09:21:34 +0330

    AuthController added api.php file done

--- src/APIAuthServiceProvider.php
+++ src/APIAuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Miladev\APIAuth;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\File;
 class APIAuthServiceProvider extends ServiceProvider
 {
     public function boot()
@@ -15,6 +16,22 @@ class APIAuthServiceProvider extends ServiceProvider
                 __DIR__."/stubs/$fileName.stub" => app_path("Http/Requests/$fileName.php"),
             ], 'stubs');
         }
+
+        $controllerFiles = glob("$stubsDirectory/*Controller.stub");
+        foreach ($controllerFiles as $file) {
+            $fileName = pathinfo($file,PATHINFO_FILENAME);
+            $this->publishes([
+                __DIR__."/stubs/$fileName.stub" => app_path("Http/Controllers/API/$fileName.php"),
+            ], 'stubs');
+        }
+
+
+
+
+        $routeFile = "$stubsDirectory/api.stub";
+        $fileName = pathinfo($routeFile, PATHINFO_FILENAME);
+
+        File::append(base_path("routes/$fileName.php"), File::get($routeFile));
     }
 
     public function register()
