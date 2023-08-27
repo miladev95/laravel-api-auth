0b7fbeb38d9569fb16d60aef134edc7c3cf6896b
Milad <miladev95@gmail.com>
2023-08-27 10:17:33 +0330

    Logout added copy file changed

--- src/APIAuthServiceProvider.php
+++ src/APIAuthServiceProvider.php
@@ -2,36 +2,43 @@
 
 namespace Miladev\APIAuth;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\ServiceProvider;
+
 class APIAuthServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        echo "im here" . PHP_EOL;
         $stubsDirectory = __DIR__ . '/stubs';
         $requestFiles = glob("$stubsDirectory/*Request.stub");
         foreach ($requestFiles as $file) {
-            $fileName = pathinfo($file,PATHINFO_FILENAME);
+            $fileName = pathinfo($file, PATHINFO_FILENAME);
             $this->publishes([
-                __DIR__."/stubs/$fileName.stub" => app_path("Http/Requests/$fileName.php"),
+                __DIR__ . "/stubs/$fileName.stub" => app_path("Http/Requests/$fileName.php"),
             ], 'stubs');
         }
 
         $controllerFiles = glob("$stubsDirectory/*Controller.stub");
         foreach ($controllerFiles as $file) {
-            $fileName = pathinfo($file,PATHINFO_FILENAME);
+            $fileName = pathinfo($file, PATHINFO_FILENAME);
             $this->publishes([
-                __DIR__."/stubs/$fileName.stub" => app_path("Http/Controllers/API/$fileName.php"),
+                __DIR__ . "/stubs/$fileName.stub" => app_path("Http/Controllers/API/$fileName.php"),
             ], 'stubs');
         }
 
 
 
-
         $routeFile = "$stubsDirectory/api.stub";
         $fileName = pathinfo($routeFile, PATHINFO_FILENAME);
+        $targetFilePath = base_path("routes/$fileName.php");
+        $contentsToAppend = file_get_contents(__DIR__ . "/stubs/$fileName.stub");
+        file_put_contents($targetFilePath, $contentsToAppend, FILE_APPEND);
 
-        File::append(base_path("routes/$fileName.php"), File::get($routeFile));
+//        $this->publishes([
+//            __DIR__ . "/stubs/$fileName.stub" => $targetFilePath,
+//        ], 'stubs');
+
     }
 
     public function register()
